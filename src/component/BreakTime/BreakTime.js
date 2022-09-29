36fcb9afa5439582754808dd6c985026f2c79d2f
shaahfardin <fardinshaah@gmail.com>
2022-09-29 19:11:19 +0600

    break time done successfully at last

--- src/component/BreakTime/BreakTime.js
+++ src/component/BreakTime/BreakTime.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 const BreakTime = (props) => {
+ 
   return (
     <div>
-      <button className="border-2 p-2 rounded-full hover:bg-red-500 hover:text-white">
+      <button
+        onClick={(e) =>props.breakTimeHandle(e)}
+        
+        className="border-2 p-2 rounded-full hover:bg-red-500 hover:text-white"
+      >
         {props.number}
       </button>
     </div>
--- src/component/Exercise/Exercise.js
+++ src/component/Exercise/Exercise.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import BreakTime from '../BreakTime/BreakTime';
+import React from "react";
+import BreakTime from "../BreakTime/BreakTime";
 
-const Exercise = ({time}) => {
-    let numbers = [10, 20, 30, 40, 50]
-    
+const Exercise = ({ time }) => {
+  let numbers = [10, 20, 30, 40, 50];
+
+  const breakTimeHandle = (e) => {
+    console.log(e.target.innerText);
+    const value = e.target.innerText;
+    const breakTime = document.getElementById("breakTime");
+    breakTime.innerText = value;
+  };
   return (
     <div>
       <div>
         <h1 className="text-2xl font-semibold text-left pl-5">Add A Break</h1>
         <div className="flex justify-between p-5 border-2 rounded-xl m-3 my-5 bg-gray-50">
           {numbers.map((number) => (
-            <BreakTime number={number}></BreakTime>
+            <BreakTime
+              breakTimeHandle={breakTimeHandle}
+              number={number}
+            ></BreakTime>
           ))}
         </div>
       </div>
@@ -21,7 +30,9 @@ const Exercise = ({time}) => {
       </div>
       <div className="flex justify-between  p-5 border-2 rounded-xl m-3  bg-gray-50">
         <div className="text-xl font-semibold">Break time</div>
-        <div className="font-extralight">totalTime</div>
+        <div id="breakTime" className="font-extralight">
+          totalTime
+        </div>
       </div>
       <button className="btn btn-success btn-wide hover:btn-success">
         Complete
